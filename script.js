7c6dc1c806d429e43c8214cacf50d2f06f1278da
Andrew Chappell <chappand@gmail.com>
2021-01-15 00:26:25 -0500

    Added dates to the future forecast cards and formatted using moment.js.

--- script.js
+++ script.js
@@ -1,28 +1,45 @@
 $(document).ready(function(){
 
-    var thisDay = moment().format("dddd, MMM DD YYYY");
-    var APIkey = "ca91147add856a9d853e5901fbfec8ad";
-    var currentQueryURL = "api.openweathermap.org/data/2.5/weather?q={city name}&appid=" + APIkey;
-    var futureQueryURL = "api.openweathermap.org/data/2.5/forecast/daily?q={city name}&cnt=5&appid=" + APIkey;
+    $("button").click(function(){
 
-    document.getElementById("city").textContent="City - " + thisDay;
+        function searchCity () {
+            var city = 
+        }
 
-    $.ajax({
-        url: currentQueryURL,
-        method: "GET"
-      }).then(function(response) {
-        console.log(response);
-      });
-
-      $.ajax({
-        url: futureQueryURL,
-        method: "GET"
-      }).then(function(response) {
-        console.log(response);
-      });
+        var thisDay = moment().format("dddd, MMM DD YYYY");
+        var tomorrow = moment().add(1, "days").format("L");
+        var twoDays = moment().add(2, "days").format("L");
+        var threeDays = moment().add(3, "days").format("L");
+        var fourDays = moment().add(4, "days").format("L");
+        var fiveDays = moment().add(5, "days").format("L");
+        var APIkey = "ca91147add856a9d853e5901fbfec8ad";
+        var currentQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=Saginaw&appid=" + APIkey;
+        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=Saginaw&appid=" + APIkey;
 
 
+        document.getElementById("city").textContent="City - " + thisDay;
+        document.getElementById("tomorrow").textContent=tomorrow;
+        document.getElementById("two").textContent=twoDays;
+        document.getElementById("three").textContent=threeDays;
+        document.getElementById("four").textContent=fourDays;
+        document.getElementById("five").textContent=fiveDays;
+
+        $.ajax({
+            url: currentQueryURL,
+            method: "GET"
+        }).then(function(response) {
+            console.log(response);
+        });
+
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function(response) {
+                console.log(response);
+            });
 
 
+
+        });
 
 });

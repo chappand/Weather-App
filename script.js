8259de507b222ff9efb6c8221a1db954a95625e6
Andrew Chappell <chappand@gmail.com>
2021-01-14 23:41:54 -0500

    Added API call for future forecast.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
     var thisDay = moment().format("dddd, MMM DD YYYY");
     var APIkey = "ca91147add856a9d853e5901fbfec8ad";
     var currentQueryURL = "api.openweathermap.org/data/2.5/weather?q={city name}&appid=" + APIkey;
+    var futureQueryURL = "api.openweathermap.org/data/2.5/forecast/daily?q={city name}&cnt=5&appid=" + APIkey;
 
     document.getElementById("city").textContent="City - " + thisDay;
 
@@ -12,5 +13,16 @@ $(document).ready(function(){
       }).then(function(response) {
         console.log(response);
       });
+
+      $.ajax({
+        url: futureQueryURL,
+        method: "GET"
+      }).then(function(response) {
+        console.log(response);
+      });
+
+
+
+
 
 });

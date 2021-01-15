fab425b333b5c0e52f77055c609265867a204016
Andrew Chappell <chappand@gmail.com>
2021-01-15 01:10:13 -0500

    Was able to add the form input city into the query url and also update the card to show what was selected. This is now without using local storage.

--- script.js
+++ script.js
@@ -2,15 +2,19 @@ $(document).ready(function(){
 
     $("button").click(function(event){
         event.preventDefault();
-        
-        function searchCity () {
-            var city = $("input").val();
-            localStorage.setItem("searchedCity", city);
-            // Need to update URL with city and need to update card title with city and empty the form and create new list items and create a new click function for the list items. Do I even need to store this in local storage, or can the value of the form just be used "on click" to fill in the appropriate fields in the HTML?
-        };
 
-            searchCity();
+        // function searchCity () {
+        //     var city = $("input").val();
+        //     localStorage.setItem("searchedCity", city);
 
+        //     searchCity();
+        // };
+
+
+// Need to update URL with city and need to update card title with city and empty the form and create new list items and create a new click function for the list items. Do I even need to store this in local storage, or can the value of the form just be used "on click" to fill in the appropriate fields in the HTML?
+
+
+        var city = $("input").val();
         var thisDay = moment().format("dddd, MMM DD YYYY");
         var tomorrow = moment().add(1, "days").format("L");
         var twoDays = moment().add(2, "days").format("L");
@@ -18,11 +22,11 @@ $(document).ready(function(){
         var fourDays = moment().add(4, "days").format("L");
         var fiveDays = moment().add(5, "days").format("L");
         var APIkey = "ca91147add856a9d853e5901fbfec8ad";
-        var currentQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=Saginaw&appid=" + APIkey;
-        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=Saginaw&appid=" + APIkey;
+        var currentQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIkey;
+        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIkey;
 
 
-        document.getElementById("city").textContent="City - " + thisDay;
+        document.getElementById("city").textContent=city + " - " + thisDay;
         document.getElementById("tomorrow").textContent=tomorrow;
         document.getElementById("two").textContent=twoDays;
         document.getElementById("three").textContent=threeDays;

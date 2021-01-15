5d18c446fad4614a0e4df4bd584d0e4aaac0eded
Andrew Chappell <chappand@gmail.com>
2021-01-14 23:38:46 -0500

    Added function to call the current day and add it to the card. Also set up the API key and query to retrieve data.

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 $(document).ready(function(){
-    // Store today's date and current hour in a variable to call on later
+
     var thisDay = moment().format("dddd, MMM DD YYYY");
+    var APIkey = "ca91147add856a9d853e5901fbfec8ad";
+    var currentQueryURL = "api.openweathermap.org/data/2.5/weather?q={city name}&appid=" + APIkey;
 
     document.getElementById("city").textContent="City - " + thisDay;
+
+    $.ajax({
+        url: currentQueryURL,
+        method: "GET"
+      }).then(function(response) {
+        console.log(response);
+      });
 
 });

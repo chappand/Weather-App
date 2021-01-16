09045e1e09141b1e2f07655ffba7b60aeabb4b0b
Andrew Chappell <chappand@gmail.com>
2021-01-16 14:53:09 -0500

    Got Readme in order.

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # Weather-App
+
+### Goal
+
+The main goal of the application is to allow a user to search for a city. When the user searches for a city, the city's current weather data, 5 day forecast, and search history are populated.
+
+To do so we needed code to:
+
+-Create a place for the user to enter data
+-Use a button to send that data to local storage and call the weather APIs
+-Use the API data to add info to the HTML to display to the user
+-Be able to refresh the page and keep the last search item's data up to display
+
+
+### Struggles
+
+The UV index was something I couldn't find. Also, the formatting of the appended items was always off. Took a long time to figure out how to make those look correct.
+
+### Screenshots
+
+Here is a screenshot for what it looks like when you open the page:
+![Start](/images/Before.png)
+
+
+Here is a screenshot with what it looks like with a randomly generated 10 character password:
+![GeneratedPassword](/images/10CharacterPassword.png)
+
+
+
+### Link
+
+Here is the link to the application:
+[Password Generator](https://chappand.github.io/Weather-App/)
--- script.js
+++ script.js
@@ -6,35 +6,48 @@ $(document).ready(function(){
 
     function updateCards () {
 
+
         var cityName = getCity();
         var thisDay = moment().format("dddd, MMM DD YYYY");
-        var tomorrow = moment().add(1, "days").format("L");
-        var twoDays = moment().add(2, "days").format("L");
-        var threeDays = moment().add(3, "days").format("L");
-        var fourDays = moment().add(4, "days").format("L");
-        var fiveDays = moment().add(5, "days").format("L");
+        // var tomorrow = moment().add(1, "days").format("L");
+        // var twoDays = moment().add(2, "days").format("L");
+        // var threeDays = moment().add(3, "days").format("L");
+        // var fourDays = moment().add(4, "days").format("L");
+        // var fiveDays = moment().add(5, "days").format("L");
         var APIkey = "ca91147add856a9d853e5901fbfec8ad";
         var currentQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=" + APIkey;
         var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&appid=" + APIkey;
 
         document.getElementById("city").textContent=cityName + " - " + thisDay;
-        document.getElementById("tomorrow").textContent=tomorrow;
-        document.getElementById("two").textContent=twoDays;
-        document.getElementById("three").textContent=threeDays;
-        document.getElementById("four").textContent=fourDays;
-        document.getElementById("five").textContent=fiveDays;
+        // document.getElementById("tomorrow").textContent=tomorrow;
+        // document.getElementById("two").textContent=twoDays;
+        // document.getElementById("three").textContent=threeDays;
+        // document.getElementById("four").textContent=fourDays;
+        // document.getElementById("five").textContent=fiveDays;
 
         $.ajax({
             url: currentQueryURL,
             method: "GET"
         }).then(function(response) {
+
+            document.getElementById("current-temp").textContent="Temperature: " + response.main.temp;
+            document.getElementById("current-humidity").textContent="Humidity: " + response.main.humidity;
+            document.getElementById("current-windspeed").textContent="Wind Speed: " + response.wind.speed;
+            // document.getElementById("current-UV").textcontent="Temperature: " + response.main.temp; will pull from another API
             console.log(response);
         });
+
 
             $.ajax({
                 url: queryURL,
                 method: "GET"
             }).then(function(response) {
+                // for loop to find exact days in the API and then do if statements to target that day and grab data APPEND CARDS
+                for(i = 0; i < response.list.length; i++) {
+                    if (response.list[i].dt_txt.includes("15:00:00")) {
+                        document.getElementById("newCard").insertAdjacentHTML('beforeend',"<div class='col-md'><div class='card smallCard'> <div class='card-body'><h6 class='card-title' id='tomorrow'>" + moment(response.list[i].dt_txt).format("L") + "</h6><p class='card-text symbol'>" + response.list[i].weather[0].icon + "</p><p class='card-text temp'>Temp:</p> <p class='card-text humidity'>Humidity:</p> </div></div></div>");
+                    }
+                }
                 console.log(response);
             });
     };
@@ -43,33 +56,35 @@ $(document).ready(function(){
     $("button").click(function(event){
         event.preventDefault();
 
+        document.getElementById("newCard").empty();
+
         function searchCity() {
             var city = $("input").val();
             localStorage.setItem("searchedCity", city);
         };
         searchCity();
 
-// Need to empty the form and create new, correctly formatted list items and create a new click function for the list items. Need to use Local storage so when you open up the page it automatically fills the last searched city in. Need to add IDs to each field that will be filled in with API data so I can add text content to contain that data. Also need to add icons with IF statements and UV coloring.
+// Need to empty the form and create new, correctly formatted list items and create a new click function for the list items. Need to fill in fields with API data. Also need to add icons with IF statements and UV coloring.
 
 
         var city = $("input").val();
         var thisDay = moment().format("dddd, MMM DD YYYY");
-        var tomorrow = moment().add(1, "days").format("L");
-        var twoDays = moment().add(2, "days").format("L");
-        var threeDays = moment().add(3, "days").format("L");
-        var fourDays = moment().add(4, "days").format("L");
-        var fiveDays = moment().add(5, "days").format("L");
+        // var tomorrow = moment().add(1, "days").format("L");
+        // var twoDays = moment().add(2, "days").format("L");
+        // var threeDays = moment().add(3, "days").format("L");
+        // var fourDays = moment().add(4, "days").format("L");
+        // var fiveDays = moment().add(5, "days").format("L");
         var APIkey = "ca91147add856a9d853e5901fbfec8ad";
         var currentQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIkey;
         var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + APIkey;
 
 
         document.getElementById("city").textContent=city + " - " + thisDay;
-        document.getElementById("tomorrow").textContent=tomorrow;
-        document.getElementById("two").textContent=twoDays;
-        document.getElementById("three").textContent=threeDays;
-        document.getElementById("four").textContent=fourDays;
-        document.getElementById("five").textContent=fiveDays;
+        // document.getElementById("tomorrow").textContent=tomorrow;
+        // document.getElementById("two").textContent=twoDays;
+        // document.getElementById("three").textContent=threeDays;
+        // document.getElementById("four").textContent=fourDays;
+        // document.getElementById("five").textContent=fiveDays;
 
         $.ajax({
             url: currentQueryURL,
